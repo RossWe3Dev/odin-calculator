2eb65d5444b517d29d930f2e7ed5642a808c00f0
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-12 23:38:59 +0200

    Add event handlers for operands, edit page style

--- script.js
+++ script.js
@@ -26,3 +26,38 @@ function operate(operator, num1, num2) {
     if (operator == '*') return multiply(num1, num2);
     if (operator == '/') return divide(num1, num2);
 };
+
+const display = document.querySelector('.result');
+let displayValue;
+
+const operand = document.querySelectorAll('.operand');
+
+for (let i = 0; i < operand.length; i++) {
+    operand[i].addEventListener('click', () => alert(operand[i].innerHTML))
+}
+
+/*
+function updateDisplay() {
+    let number = operand.innerHTML;
+    alert(number);
+}
+*/
+
+//! Buttons elements
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const zero = document.querySelector('#zero');
+
+const addOp = document.querySelector('#add');
+const subtractOp = document.querySelector('#subtract');
+const multiplyOp = document.querySelector('#multiply');
+const divideOp = document.querySelector('#divide');
+const equal = document.querySelector('#equal');
+const clear = document.querySelector('#clear');
