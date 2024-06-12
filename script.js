47a85ed1cd9a2ba07d0c439b17cccc0a16076e79
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-12 22:10:14 +0200

    Add basic operations' function

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('Hello World!');
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+let firstNum;
+let operator;
+let secondNum;
+
+function operate(operator, num1, num2) {
+    if (operator == '+') return add(num1, num2);
+    if (operator == '-') return subtract(num1, num2);
+    if (operator == '*') return multiply(num1, num2);
+    if (operator == '/') return divide(num1, num2);
+};
