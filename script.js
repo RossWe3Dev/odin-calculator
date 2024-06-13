1aa06847c8a572607f182552fb455fe0ab4adecc
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-13 11:55:19 +0200

    Complete operation logic and display updates
    Added event handler to equal sign that calls the operate() function.
    The logic is to split the displayValue string, using a regex, into a
    temporary array with two items. The items are then converted into
    numbers and set as firstNum and secondNum values.
    Modified the operate() function so that it rounds the result to at most
    two decimal places and it now displays an error if trying to divide by
    zero.
    Updated html.index and css.style for consistency and better readability.

--- script.js
+++ script.js
@@ -12,10 +12,19 @@ function divide(a, b) {
 };
 
 function operate(operator, num1, num2) {
-    if (operator == '+') return add(num1, num2);
-    if (operator == '-') return subtract(num1, num2);
-    if (operator == '*') return multiply(num1, num2);
-    if (operator == '/') return divide(num1, num2);
+    let result;
+    if (operator == '+') {
+        result = add(num1, num2)
+    } else if (operator == '-') {
+        result = subtract(num1, num2)
+    } else if (operator == '×') {
+        result = multiply(num1, num2)
+    } else if (operator == '÷') {
+        if (num2 === 0) return 'Math ERROR';
+        result = divide(num1, num2)
+    };
+    let resultRounded = Math.round((result + Number.EPSILON) * 100) / 100;
+    return resultRounded;
 };
 
 let firstNum;
@@ -25,18 +34,42 @@ let secondNum;
 const display = document.querySelector('.result');
 let displayValue = '';
 
-const operand = document.querySelectorAll('.operand');
-for (let i = 0; i < operand.length; i++) {
-    operand[i].addEventListener('click', function () {
-        displayValue += operand[i].innerText;
+const operandBtn = document.querySelectorAll('.operand');
+for (let i = 0; i < operandBtn.length; i++) {
+    operandBtn[i].addEventListener('click', function () {
+        displayValue += operandBtn[i].innerText;
         updateDisplayValue();
     })
-}
+};
 
 function updateDisplayValue() {
     display.textContent = displayValue;
-}
+};
 
+const operatorBtn = document.querySelectorAll('.operator');
+for (let i = 0; i < operatorBtn.length; i++) {
+    operatorBtn[i].addEventListener('click', function () {
+        operator = operatorBtn[i].innerText;
+        displayValue += operatorBtn[i].innerText;
+        updateDisplayValue();
+    })
+};
+
+const history = document.querySelector('.history');
+const operateBtn = document.querySelector('.operate');
+operateBtn.addEventListener('click', () => {
+    history.innerText = displayValue;
+    let arr = display.innerText.split(/[\+|\-|\×|\÷]/);
+    firstNum = +arr[0];
+    secondNum = +arr[1];
+    displayValue = operate(operator, firstNum, secondNum);
+    updateDisplayValue();
+    console.log(displayValue);
+    console.log(firstNum, secondNum);
+});
+
+
+/*
 //! Buttons elements
 const one = document.querySelector('#one');
 const two = document.querySelector('#two');
@@ -55,3 +88,4 @@ const multiplyOp = document.querySelector('#multiply');
 const divideOp = document.querySelector('#divide');
 const equal = document.querySelector('#equal');
 const clear = document.querySelector('#clear');
+*/
