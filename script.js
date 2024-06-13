c6676262c7bcdc005ef4813acc853c82ee90b711
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-13 21:55:15 +0200

    Add point button with its logic, clean the code

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-function add(a, b) {
-    return a + b;
-};
-function subtract(a, b) {
-    return a - b;
-};
-function multiply(a, b) {
-    return a * b;
-};
-function divide(a, b) {
-    return a / b;
-};
-
-function operate(operator, num1, num2) {
-    if (operator === '+') return add(num1, num2);
-    if (operator === '-') return subtract(num1, num2);
-    if (operator === '×') return multiply(num1, num2);
-    if (operator === '÷') return divide(num1, num2);
-};
-
-function roundResult(result) {
-    return Math.round((result + Number.EPSILON) * 1000) / 1000;
-}
-
 let firstNum;
 let operator = null;
 let secondNum;
@@ -30,32 +6,13 @@ let shouldResetDisplay = false;
 const display = document.querySelector('.result');
 const historyDisplay = document.querySelector('.history');
 const operandBtn = document.querySelectorAll('.operand');
+const pointBtn = document.querySelector('.point');
 const operatorBtn = document.querySelectorAll('.operator');
 const operateBtn = document.querySelector('.operate');
 const clearBtn = document.querySelector('.clear');
 const deleteBtn = document.querySelector('.delete');
 
-//* Functions
-function resetDisplay() {
-    display.textContent = '';
-    shouldResetDisplay = false
-}
-
-function evaluate() {
-    if (operator == null || shouldResetDisplay) return;
-
-    secondNum = Number(display.textContent);
-    if (secondNum === 0 && operator === '÷') {
-        shouldResetDisplay = true;
-        return display.textContent = 'Math ERROR'
-    };
-
-    display.textContent = roundResult(operate(operator, firstNum, secondNum));
-    historyDisplay.textContent = `${firstNum}${operator}${secondNum}=`;
-    operator = null;
-}
-
-//* Event Listeners
+//* Event Handlers
 operandBtn.forEach((button) => {
     button.addEventListener('click', () => {
         if (display.textContent === '0' || shouldResetDisplay) resetDisplay();
@@ -63,6 +20,13 @@ operandBtn.forEach((button) => {
         display.textContent += button.textContent
     })
 });
+
+pointBtn.addEventListener('click', () => {
+    if (shouldResetDisplay) resetDisplay();
+    if (display.textContent === '') display.textContent = '0';
+    if (display.textContent.includes('.')) return;
+    display.textContent += pointBtn.textContent;
+})
 
 operatorBtn.forEach((button) => {
     button.addEventListener('click', function () {
@@ -91,16 +55,47 @@ deleteBtn.addEventListener('click', () => {
 });
 
 
-/*
-//! Buttons elements f0r keyboard support
-const one = document.querySelector('#one');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
-const seven = document.querySelector('#seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
-const zero = document.querySelector('#zero');
-*/
+//* Functions
+function resetDisplay() {
+    display.textContent = '';
+    shouldResetDisplay = false
+}
+
+function evaluate() {
+    if (operator == null || shouldResetDisplay) return;
+
+    secondNum = Number(display.textContent);
+    if (secondNum === 0 && operator === '÷') {
+        shouldResetDisplay = true;
+        return display.textContent = 'Math ERROR'
+    };
+
+    display.textContent = roundResult(operate(operator, firstNum, secondNum));
+    historyDisplay.textContent = `${firstNum}${operator}${secondNum}=`;
+    operator = null;
+}
+
+//* Math Logic
+function roundResult(result) {
+    return Math.round((result + Number.EPSILON) * 1000) / 1000;
+}
+
+function operate(operator, num1, num2) {
+    if (operator === '+') return add(num1, num2);
+    if (operator === '-') return subtract(num1, num2);
+    if (operator === '×') return multiply(num1, num2);
+    if (operator === '÷') return divide(num1, num2);
+};
+
+function add(a, b) {
+    return a + b;
+};
+function subtract(a, b) {
+    return a - b;
+};
+function multiply(a, b) {
+    return a * b;
+};
+function divide(a, b) {
+    return a / b;
+};
